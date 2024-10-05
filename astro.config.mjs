c8234420a17668fe4378ec7f2bec9719b4fd6549
tsukimi <128211112+hikawi@users.noreply.github.com>
2024-10-05 16:57:43 +0700

    oops forgot image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    imageService: true,
+  }),
 });
